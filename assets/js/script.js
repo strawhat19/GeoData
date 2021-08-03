26ae04c68c3bac7705c7f40dc6f56382531ecee3
Rakib Ahmed <rakib987@gmail.com>
2021-08-03 02:05:25 -0400

    Tracking City Time

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,11 +30,24 @@ searchButton.on('click',function(event) {
     .then(function(response) {
         return response.json();
     }).then(function(data) {
-        console.log(data);
-        cityNameText.html(' ' + data.name);
-        temperature.html(' ' + data.main.temp);
-        humidity.html(' ' + data.main.humidity);
-        wind.html(' ' + data.wind.speed);
+        var fahrenheit = Math.floor((data.main.temp - 273.15)* 1.8 + 32.00);
+        cityNameText.html(data.name);
+        temperature.html(fahrenheit + '° F');
+        humidity.html(data.main.humidity);
+        wind.html(data.wind.speed);
+
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        var latlonLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        fetch(latlonLink).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            uvIndex.html(data.current.uvi);
+            console.log(data);
+            var currentTime = moment.unix(data.current.dt).format('dddd, MMMM Do YYYY, h:mm:ss a');
+            var currentTimeEl = $(`<span class="currentTime"> - ${currentTime} </span>`);
+            cityNameText.append(currentTimeEl);
+        })
     })
     // location.reload(true);
 })
@@ -54,10 +67,23 @@ locationButtons.on('click',function(event) {
     .then(function(response) {
         return response.json();
     }).then(function(data) {
-        console.log(data);
-        cityNameText.html(' ' + data.name);
-        temperature.html(' ' + data.main.temp);
-        humidity.html(' ' + data.main.humidity);
-        wind.html(' ' + data.wind.speed);
+        var fahrenheit = Math.floor((data.main.temp - 273.15)* 1.8 + 32.00);
+        cityNameText.html(data.name);
+        temperature.html(fahrenheit + '° F');
+        humidity.html(data.main.humidity);
+        wind.html(data.wind.speed);
+
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        var latlonLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        fetch(latlonLink).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            uvIndex.html(data.current.uvi);
+            console.log(data);
+            var currentTime = moment.unix(data.current.dt).format('dddd, MMMM Do YYYY, h:mm:ss a');
+            var currentTimeEl = $(`<span class="currentTime"> - ${currentTime} </span>`);
+            cityNameText.append(currentTimeEl);
+        })
     })
 })
