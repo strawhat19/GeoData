1e2cb028948493437114451cf323999ca127a9b5
Rakib Ahmed <rakib987@gmail.com>
2021-08-03 06:37:28 -0400

    Google Maps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Weather App
 console.log('Weather App!');
 
+function initmap(){
+    var options = {
+        zoom: 8,
+        center: {lat:42.3601,lng:-71.0589}
+    }
+
+    var map = new google.maps.Map(document.getElementById('map'),options);
+}
+
+initmap();
+
 var apiKey = 'ce5300e7acaa327ad655b8a21d5130d8';
 var cityName = '';
 var searchInput = $('#search');
