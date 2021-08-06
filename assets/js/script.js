7843e47dfd3799d190a611979fd40f31d56deb84
Rakib Ahmed <rakib987@gmail.com>
2021-08-06 02:07:53 -0400

    Input Validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,82 +42,91 @@ var coordinates = $('.coords');
 searchButton.on('click',function(event) {
     event.preventDefault();
     var searchInputValue = searchInput.val();
-    cities.push(searchInputValue);
-    cities.splice(11);
-    localStorage.setItem('Cities History', JSON.stringify(cities));
-    var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=${apiKey}`;
-    // Fetch first dataset
-    fetch(requestURL)
-    .then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        console.log(data);
-        // Converting Temperature from Kelvin to Fahrenheit
-        var fahrenheit = Math.floor((data.main.temp - 273.15)* 1.8 + 32.00);
-        cityNameText.html(data.name + ', ' + data.sys.country);
-        temperature.html(fahrenheit + '° F');
-        humidity.html(data.main.humidity);
-        wind.html(data.wind.speed);
-        coordinates.html(Math.floor(data.coord.lat) + ', ' + Math.floor(data.coord.lon));
-
-        // Initializing Coords
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-        // Reinitializing new map
-        function initmap(){
-            var options = {
-                zoom: 9,
-                center: {lat:lat,lng:lon}
-            }
-            
-            var map = new google.maps.Map(document.getElementById('map'),options);
-        } // Reinvoking Map with new Coords
-        initmap();
-
-        // Fetching 2nd dataset with new Lat Lon and OneCall API
-        var latlonLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-        fetch(latlonLink).then(function(response) {
+    if (!searchInputValue) {
+        alert('no');
+        return;
+    } else {
+        var requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&appid=${apiKey}`;
+        // Fetch first dataset
+        fetch(requestURL)
+        .then(function(response) {
             return response.json();
         }).then(function(data) {
-            uvIndex.html(data.current.uvi);
             console.log(data);
-            var currentTime = moment.unix(data.current.dt).format('dddd, MMMM Do YYYY, h:mm a');
-            var currentTimeEl = $(`<span class="currentTime"> - ${currentTime} </span>`);
-            cityNameText.append(currentTimeEl);
-            cardContainer.html('');
-            conditionText.html(data.daily[0].weather[0].main);
-            // 5 day forecast
-            for (var i = 1; i < 6; i++) {
-                var fullDates = moment.unix(data.daily[i].dt).format('MMMM Do');
-                var day = moment.unix(data.daily[i].dt).format('dddd');
-                var mainIcon = data.daily[0].weather[0].icon;
-                var mainIconLink = `https://openweathermap.org/img/wn/${mainIcon}@2x.png`;
-                condition.addClass('conditionImageInverted');
-                condition.attr('src',mainIconLink);
-                var icon = data.daily[i].weather[0].icon;
-                var iconLink = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-                // Creating Forecast Cards
-                var foreCastCards = $(`
-                <div class="card">
-                  <div class="dateIcon"><h4 class="date">${fullDates}</h4><img class="icon" src="${iconLink}"></div>
-                    <h3 class="dayText">${day}</h3>
-                    <div class="spanContainer">
-                        <div class="stat">Temperature: 
-                            <span class="cardTemperature">${Math.floor((data.daily[i].temp.max - 273.15)* 1.8 + 32.00)} ° F</span>
-                        </div>
-                        <div class="stat">Wind Speed: 
-                            <span class="cardWind">${data.daily[i].wind_speed}</span>
-                        </div>
-                        <div class="stat">Humidity: 
-                            <span class="cardHumidity">${data.daily[i].humidity}</span>
-                        </div>
-                    </div>
-                </div>`);
-                cardContainer.append(foreCastCards);
+            if (data.cod === '404') {
+                alert('City Not Found.');
+                return;
+            } else {
+                cities.push(searchInputValue);
+                cities.splice(11);
+                localStorage.setItem('Cities History', JSON.stringify(cities));
+                // Converting Temperature from Kelvin to Fahrenheit
+                var fahrenheit = Math.floor((data.main.temp - 273.15)* 1.8 + 32.00);
+                cityNameText.html(data.name + ', ' + data.sys.country);
+                temperature.html(fahrenheit + '° F');
+                humidity.html(data.main.humidity);
+                wind.html(data.wind.speed);
+                coordinates.html(Math.floor(data.coord.lat) + ', ' + Math.floor(data.coord.lon));
+
+                // Initializing Coords
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+                // Reinitializing new map
+                function initmap(){
+                    var options = {
+                        zoom: 9,
+                        center: {lat:lat,lng:lon}
+                    }
+                    
+                    var map = new google.maps.Map(document.getElementById('map'),options);
+                } // Reinvoking Map with new Coords
+                initmap();
+
+                // Fetching 2nd dataset with new Lat Lon and OneCall API
+                var latlonLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+                fetch(latlonLink).then(function(response) {
+                    return response.json();
+                }).then(function(data) {
+                    uvIndex.html(data.current.uvi);
+                    console.log(data);
+                    var currentTime = moment.unix(data.current.dt).format('dddd, MMMM Do YYYY, h:mm a');
+                    var currentTimeEl = $(`<span class="currentTime"> - ${currentTime} </span>`);
+                    cityNameText.append(currentTimeEl);
+                    cardContainer.html('');
+                    conditionText.html(data.daily[0].weather[0].main);
+                    // 5 day forecast
+                    for (var i = 1; i < 6; i++) {
+                        var fullDates = moment.unix(data.daily[i].dt).format('MMMM Do');
+                        var day = moment.unix(data.daily[i].dt).format('dddd');
+                        var mainIcon = data.daily[0].weather[0].icon;
+                        var mainIconLink = `https://openweathermap.org/img/wn/${mainIcon}@2x.png`;
+                        condition.addClass('conditionImageInverted');
+                        condition.attr('src',mainIconLink);
+                        var icon = data.daily[i].weather[0].icon;
+                        var iconLink = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+                        // Creating Forecast Cards
+                        var foreCastCards = $(`
+                        <div class="card">
+                        <div class="dateIcon"><h4 class="date">${fullDates}</h4><img class="icon" src="${iconLink}"></div>
+                            <h3 class="dayText">${day}</h3>
+                            <div class="spanContainer">
+                                <div class="stat">Temperature: 
+                                    <span class="cardTemperature">${Math.floor((data.daily[i].temp.max - 273.15)* 1.8 + 32.00)} ° F</span>
+                                </div>
+                                <div class="stat">Wind Speed: 
+                                    <span class="cardWind">${data.daily[i].wind_speed}</span>
+                                </div>
+                                <div class="stat">Humidity: 
+                                    <span class="cardHumidity">${data.daily[i].humidity}</span>
+                                </div>
+                            </div>
+                        </div>`);
+                        cardContainer.append(foreCastCards);
+                    }
+                })
             }
         })
-    })
-    // location.reload(true);
+    }
 })
 
 // Creating Buttons for each City Stored in Cities Array
